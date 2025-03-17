33510628ec7ebab4a8768ab4ac99adee376f3530
Daniel Batista <dani_batista20@hotmail.com>
2025-03-17 19:16:41 +0000

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen gap-10">
+      <h1 className="text-5xl dark:bg-amber-500 bg-pink-500">Hello World</h1>
+    </div>
+  )
+}
