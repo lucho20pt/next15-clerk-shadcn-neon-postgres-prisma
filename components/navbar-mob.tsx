28f36c1f02be236fc5a6323426e427f7f944b0fe
Daniel Batista <dani_batista20@hotmail.com>
2025-03-19 00:39:49 +0000

    feat: enhance NavbarMob with user-specific links and UserLogin component

--- components/navbar-mob.tsx
+++ components/navbar-mob.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+import { currentUser } from '@clerk/nextjs/server'
 import {
   Sheet,
   SheetContent,
@@ -7,21 +9,49 @@ import {
   SheetTrigger,
 } from '@/components/ui/sheet'
 import { Sidebar } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import UserLogin from '@/components/user-login'
 
-const NavbarMob = () => {
+const NavbarMob = async () => {
+  const user = await currentUser()
+
   return (
     <Sheet>
-      <SheetTrigger className="flex md:hidden hover:cursor-pointer">
-        <Sidebar />
+      <SheetTrigger asChild className="flex md:hidden">
+        <Button variant="outline">
+          <Sidebar />
+        </Button>
       </SheetTrigger>
-      <SheetContent>
+      <SheetContent side="right">
         <SheetHeader>
-          <SheetTitle>Are you absolutely sure?</SheetTitle>
-          <SheetDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </SheetDescription>
+          <SheetTitle>Menu</SheetTitle>
         </SheetHeader>
+        <SheetDescription asChild>
+          <ul className="flex flex-col items-center justify-center gap-4 font-bold text-xl">
+            <li className="hover:opacity-80 hover:underline">
+              <Link href="/" aria-label="Home">
+                Home
+              </Link>
+            </li>
+            {user ? (
+              <>
+                <li className="hover:opacity-80 hover:underline">
+                  <Link href="/notifications" aria-label="Notifications">
+                    Notifications
+                  </Link>
+                </li>
+                <li className="hover:opacity-80 hover:underline">
+                  <Link href="/profile" aria-label="Profile">
+                    Profile
+                  </Link>
+                </li>
+              </>
+            ) : null}
+            <>
+              <li><UserLogin /></li>
+            </>
+          </ul>
+        </SheetDescription>
       </SheetContent>
     </Sheet>
   )
