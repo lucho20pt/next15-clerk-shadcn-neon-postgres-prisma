99ec6b3ada7c5212e9c4120b4e319a7f4041cd40
Daniel Batista <dani_batista20@hotmail.com>
2025-04-02 23:56:19 +0100

    feat: enhance Authenticated component to display user information and image

--- components/authenticated.tsx
+++ components/authenticated.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { currentUser } from '@clerk/nextjs/server'
+import Image from 'next/image'
 
 const Authenticated = async () => {
+  const user = await currentUser()
+  const userFirstName = user?.firstName || null
+  const userLastName = user?.lastName || null
+  const userImageUrl = user?.imageUrl || null
   return (
-    <div>
-      <h2>User</h2>
-    </div>
+    <section className="flex flex-col items-center justify-center gap-4 bg-accent">
+      <Image
+        src={userImageUrl || './default-image.png'}
+        alt={`${userFirstName || 'User'} ${userLastName || ''}`}
+        width={120}
+        height={120}
+      />
+      <aside className="p-2 text-accent-foreground">
+        <h2>
+          <strong>Wellcome</strong>
+        </h2>
+        <p>
+          {userFirstName} {userLastName}
+        </p>
+      </aside>
+    </section>
   )
 }
 
