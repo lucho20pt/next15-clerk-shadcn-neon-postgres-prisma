27ff1c12f56ecd039ebe26296d89515700b5905d
Daniel Batista <dani_batista20@hotmail.com>
2025-04-04 12:39:32 +0100

    feat: update Header component title and add descriptive code snippet

--- components/header.tsx
+++ components/header.tsx
@@ -15,8 +15,11 @@ const Header = async () => {
       className="flex flex-row justify-around items-center p-4 gap-4 min-h-16 flex-wrap 
     bg-white text-black dark:bg-black dark:text-white border border-b-amber-500 dark:border-b-pink-500"
     >
-      <h1 className="flex font-bold text-lg justify-start">
-        <Link href="/">SocialHub</Link>
+      <h1 className="flex flex-col font-bold text-lg justify-start">
+        <Link href="/">BoilerPlate</Link>
+        <code className="flex text-[0.6rem] flex-wrap">
+          next15 clerk shadcn <br /> neon postgres prisma
+        </code>
       </h1>
       <nav className="flex flex-row items-center justify-end gap-3 md:gap-6">
         <ThemeToggle />
